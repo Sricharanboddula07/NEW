a7b083504e9e6c140d7bdfba764edb3c4fa5d80d
Sricharanboddula <sricharanboddula@gmail.com>
2025-07-28 10:50:05 +0530

    initialCommit

--- TASK/src/main.jsx
+++ TASK/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles.css'
+import VotingApp from './VotingApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <VotingApp/>
   </StrictMode>,
 )
